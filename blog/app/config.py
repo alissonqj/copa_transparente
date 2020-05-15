ed56dbd5520fd7722ca653b45517488d7f64310d
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-05-14 21:43:04 -0300

    Adiciona parte 3 do blog em Flask

---
+++ blog/app/config.py
@@ -0,0 +1,13 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# SQLITE
+# MYSQL, POSTGRES, ORACLE
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "voce-nunca-adivinhara"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- blog/app/routes.py
+++ blog/app/routes.py
@@ -1,18 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Vinicius'}
+    user = {"username": "Vinicius"}
     posts = [
-        {
-            'author': {'username': 'Vinicius'},
-            'body': 'Ótimo dia em Florianópolis'
-        },
-        {
-            'author': {'username': 'Jaiminho'},
-            'body': 'Dia chuvoso em Tamgamandápio'
-        }
+        {"author": {"username": "Vinicius"}, "body": "Ótimo dia em Florianópolis"},
+        {"author": {"username": "Jaiminho"}, "body": "Dia chuvoso em Tamgamandápio"},
     ]
-    return render_template('index.html', title='Página Inicial', user=user, posts=posts)
+    return render_template("index.html", title="Página Inicial", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "Login solicitado pelo usuário {}, me lembre={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for("index"))
+
+    return render_template("login.html", title="Sign In", form=form)
