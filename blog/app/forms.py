a3490fcb977f37e47e1a8a97fe2e78ff7fe3948d
Vinícius Melo <vinimmelo.dev@gmail.com>
2020-05-21 21:44:11 -0300

    Adiciona novas funcionalidades ao blog
    - Novos formulários
    - Novos templates
    - Atualiações de última vez visto
    - E Muito mais

--- blog/app/forms.py
+++ blog/app/forms.py
@@ -1,10 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
+from app.models import User
 
 
 class LoginForm(FlaskForm):
     username = StringField("Usuário", validators=[DataRequired()])
     password = PasswordField("Senha", validators=[DataRequired()])
     remember_me = BooleanField("Me lembre")
+    submit = SubmitField("Submit")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Usuário", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(min=8)])
+    password2 = PasswordField(
+        "Repita a senha", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Registrar-se")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Por favor use um usuário diferente")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Por favor use um email diferente.")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Usuário", validators=[DataRequired()])
+    about_me = TextAreaField("Sobre mim", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
--- blog/app/routes.py
+++ blog/app/routes.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from datetime import datetime
+
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
-from flask_login import current_user, login_user
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Vinicius"}
     posts = [
         {"author": {"username": "Vinicius"}, "body": "Ótimo dia em Florianópolis"},
         {"author": {"username": "Jaiminho"}, "body": "Dia chuvoso em Tamgamandápio"},
     ]
-    return render_template("index.html", title="Página Inicial", user=user, posts=posts)
+    return render_template("index.html", title="Página Inicial", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -27,7 +30,67 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash("Usuário ou senha inválida")
             return redirect(url_for("login"))
+        # request em flask
+        # é toda meta informação que você recebe
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
         return redirect(url_for("index"))
 
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Parabéns, agora você é um usuário registrado!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "TEst post #1"},
+        {"author": user, "body": "test post #2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Suas modificações foram salvas.")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edição de Perfil", form=form)
